bb94520b53bb7ae6b48ceb1282420a4701ee04e6
Andrei Boar <andrey.boar@gmail.com>
2016-11-21 19:49:35 +0100

    allow multiple sources

--- main.go
+++ main.go
@@ -6,11 +6,15 @@ import (
 )
 
 func main() {
-	jobs := make([]sources.Job, 0)
-	for _, s := range sources.All() {
-		jobs = append(jobs, sources.SearchFor("php", s)...)
-	}
+	topics := make([]string, 2)
+	topics = append(topics, "php", "golang")
 
+	jobs := make([]sources.Job, 0)
+	for _, topic := range topics {
+		for _, s := range sources.All() {
+			jobs = append(jobs, sources.SearchFor(topic, s)...)
+		}
+	}
 	for k, v := range jobs {
 		fmt.Println(k, v.Title, v.Url)
 	}
