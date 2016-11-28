8cabba7a9faf1594b2cc8695167bdf259bdc84d0
Andrei Boar <andrei.boar@movago.com>
2016-11-28 16:54:20 +0100

    prepare stackoverflow date calculation

--- main.go
+++ main.go
@@ -6,11 +6,16 @@ import (
 	"github.com/zuzuleinen/jobber/commands"
 	"github.com/zuzuleinen/jobber/database"
 	"os"
+	"time"
+	"strings"
 )
 
 func main() {
 	db := database.Connect()
 	defer db.Close()
+
+	dateAdded := "2 hour ago"
+	fmt.Println(getTime(dateAdded))
 
 	if len(os.Args) < 2 {
 		fmt.Println("Use `init` or `search` commands.")
@@ -28,4 +33,32 @@ func main() {
 	case "search":
 		commands.SearchJobs(db)
 	}
+}
+
+func getTime(d string) time.Time {
+	if strings.Contains(d, "hours") {
+		d = strings.Replace(d, "hours", "h", -1)
+	}
+	if strings.Contains(d, "hour") {
+		d = strings.Replace(d, "hour", "h", -1)
+	}
+
+	//max unit is h so this bellow needs to be refactor
+	if strings.Contains(d, "yesterday") {
+		d = strings.Replace(d, "yesterday", "1d", -1)
+	}
+	if strings.Contains(d, "days") {
+		d = strings.Replace(d, "days", "d", -1)
+	}
+	if strings.Contains(d, "week") {
+		d = strings.Replace(d, "days", "d", -1)
+	}
+
+	d = strings.Replace(d, "ago", "", -1)
+	d = strings.Replace(d, " ", "", -1)
+	d = "-" + d
+	fmt.Println(d)
+	duration, _ := time.ParseDuration(d)
+
+	return time.Now().Add(duration)
 }
